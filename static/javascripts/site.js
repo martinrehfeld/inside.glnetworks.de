3e96ac9b5a359b11447ec5ec89f588751e85a707
Martin Rehfeld <martin.rehfeld@glnetworks.de>
2010-06-27 01:45:23 +0200

    basic grid styling, added articles and DISQUS

---
+++ static/javascripts/site.js
@@ -0,0 +1,7 @@
+// support HTML5 elements in older browsers
+$.each("header nav article section footer aside time figure figcaption".split(" "), function(i, element) {
+  document.createElement(element);
+});
+
+// add browser identifier to html tag
+$('html').addClass($.browser.webkit ? 'webkit' : $.browser.msie ? 'trident' : $.browser.opera ? 'presto' : $.browser.mozilla ? 'gecko' : null);
