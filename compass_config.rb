8199b0a3782cefd8cd5ea9dc7d3cf0b02b472c9d
Martin Rehfeld <martin.rehfeld@glnetworks.de>
2010-06-26 23:02:24 +0200

    add 960.gs compass plugin

--- compass_config.rb
+++ compass_config.rb
@@ -1,3 +1,5 @@
+require 'ninesixty'
+
 project_path = File.dirname(__FILE__)
 http_path    = '/'
 output_style = :expanded
@@ -5,3 +7,10 @@ sass_dir     = 'content/stylesheets'
 css_dir      = 'output/stylesheets'
 images_dir   = 'output/images'
 http_images_path = '/images'
+asset_cache_buster do |http_path, real_path|
+  if File.exists?(real_path)
+    File.mtime(real_path).strftime("%s")
+  else
+    "v=1"
+  end
+end
