95b17951b42ef0abc3b5debea3719496fb14592c
Martin Rehfeld <martin.rehfeld@glnetworks.de>
2011-02-08 15:50:58 +0100

    preserve asset modification times

--- lib/default.rb
+++ lib/default.rb
@@ -61,7 +61,7 @@ end
 
 # Copy static assets outside of content instead of having nanoc3 process them.
 def copy_static
-  FileUtils.cp_r 'static/.', 'output/'
+  FileUtils.cp_r 'static/.', 'output/', :preserve => true
 end
 
 def apply_filters
